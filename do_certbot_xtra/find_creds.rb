268d4c52a35ac23e6b35ff0d8f51a941dddb2e93
IotaSpencer <me@iotaspencer.me>
2024-03-23 13:22:43 -0400

    more testing

--- do_certbot_xtra/find_creds.rb
+++ do_certbot_xtra/find_creds.rb
@@ -1,37 +1,39 @@
 # Do testing on what exists
-def get_creds_file
-  places_to_check = {
-    secrets: "~/.secrets/certbot/cloudflare.ini",
-    cloudflare_dir: "~/.cloudflare/cloudflare.cfg"
+class Creds
+  def get_creds_file
+    places_to_check = {
+      secrets: "~/.secrets/certbot/cloudflare.ini",
+      cloudflare_dir: "~/.cloudflare/cloudflare.cfg"
 
-  }
-  outcome = {
-    secrets: nil,
-    cloudflare_dir: nil,
+    }
+    outcome = {
+      secrets: nil,
+      cloudflare_dir: nil,
 
-  }
-  credentials_file = nil
-  for name, path in places_to_check
-    if File.exist? File.expand_path(path)
-      outcome[name] = true
-      puts 'found secrets'
-    else
-      outcome[name] = false
-      puts 'found cloudflare_dir'
+    }
+    credentials_file = nil
+    for name, path in places_to_check
+      if File.exist? File.expand_path(path)
+        outcome[name] = true
+        puts 'found secrets'
+      else
+        outcome[name] = false
+        puts 'found cloudflare_dir'
+      end
     end
-  end
-  outcome_secrets = outcome[:secrets]
-  outcome_cf_dir = outcome[:cloudflare_dir]
-  if outcome
-    if outcome_secrets == true
-      credentials_file = File.expand_path(places_to_check[:secrets])
-    elsif outcome_secrets == false && outcome_cf_dir == true
-      credentials_file = File.expand_path(places_to_check[:cloudflare_dir])
-    elsif outcome_cf_dir == false && outcome_secrets == false
-      credentials_file = nil
-      STDERR.puts "Unable to find valid credentials file"
-      STDERR.puts "Stopping!..."
-      exit(1)
+    outcome_secrets = outcome[:secrets]
+    outcome_cf_dir = outcome[:cloudflare_dir]
+    if outcome
+      if outcome_secrets == true
+        credentials_file = File.expand_path(places_to_check[:secrets])
+      elsif outcome_secrets == false && outcome_cf_dir == true
+        credentials_file = File.expand_path(places_to_check[:cloudflare_dir])
+      elsif outcome_cf_dir == false && outcome_secrets == false
+        credentials_file = nil
+        STDERR.puts "Unable to find valid credentials file"
+        STDERR.puts "Stopping!..."
+        exit(1)
+      end
     end
   end
 end
